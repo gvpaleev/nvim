61987d24b0386886939b6cfb5e2feeca8261fdf4
Gennady Paleev <gv.paleev@gmail.com>
2025-04-15 21:55:38 +0100

    init

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Mason plugins
+
+---@type LazySpec
+return {
+  -- {
+  --   "jose-elias-alvarez/null-ls.nvim",
+  --   opts = function(_, opts)
+  --     opts.should_attach = function(bufnr)
+  --       local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+  --       return ft ~= "typescript" -- отключаем null-ls для TypeScript
+  --     end
+  --   end,
+  -- },
+}
