806a12603653feff90640bcc6dc37d1252325fff
Gennady Paleev <gv.paleev@gmail.com>
2025-04-26 02:42:41 +0300

    draft

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,29 +4,37 @@
 
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode with jj" })
 
-local kube_utils_mappings = {
-  { "<leader>k", group = "Kubernetes" }, -- Main title for all Kubernetes related commands
-  -- Helm Commands
-  { "<leader>kh", group = "Helm" },
-  { "<leader>khT", "<cmd>HelmDryRun<CR>", desc = "Helm DryRun Buffer" },
-  { "<leader>khb", "<cmd>HelmDependencyBuildFromBuffer<CR>", desc = "Helm Dependency Build" },
-  { "<leader>khd", "<cmd>HelmDeployFromBuffer<CR>", desc = "Helm Deploy Buffer to Context" },
-  { "<leader>khr", "<cmd>RemoveDeployment<CR>", desc = "Helm Remove Deployment From Buffer" },
-  { "<leader>kht", "<cmd>HelmTemplateFromBuffer<CR>", desc = "Helm Template From Buffer" },
-  { "<leader>khu", "<cmd>HelmDependencyUpdateFromBuffer<CR>", desc = "Helm Dependency Update" },
-  -- Kubectl Commands
-  { "<leader>kk", group = "Kubectl" },
-  { "<leader>kkC", "<cmd>SelectSplitCRD<CR>", desc = "Download CRD Split" },
-  { "<leader>kkD", "<cmd>DeleteNamespace<CR>", desc = "Kubectl Delete Namespace" },
-  { "<leader>kkK", "<cmd>OpenK9s<CR>", desc = "Open K9s" },
-  { "<leader>kka", "<cmd>KubectlApplyFromBuffer<CR>", desc = "Kubectl Apply From Buffer" },
-  { "<leader>kkc", "<cmd>SelectCRD<CR>", desc = "Download CRD" },
-  { "<leader>kkk", "<cmd>OpenK9sSplit<CR>", desc = "Split View K9s" },
-  { "<leader>kkl", "<cmd>ToggleYamlHelm<CR>", desc = "Toggle YAML/Helm" },
-  -- Logs Commands
-  { "<leader>kl", group = "Logs" },
-  { "<leader>klf", "<cmd>JsonFormatLogs<CR>", desc = "Format JSON" },
-  { "<leader>klv", "<cmd>ViewPodLogs<CR>", desc = "View Pod Logs" },
-}
+-- local kube_utils_mappings = {
+--   { "<leader>k", group = "Kubernetes" }, -- Main title for all Kubernetes related commands
+-- -- Helm Commands
+-- { "<leader>kh", group = "Helm" },
+-- { "<leader>khT", "<cmd>HelmDryRun<CR>", desc = "Helm DryRun Buffer" },
+-- { "<leader>khb", "<cmd>HelmDependencyBuildFromBuffer<CR>", desc = "Helm Dependency Build" },
+-- { "<leader>khd", "<cmd>HelmDeployFromBuffer<CR>", desc = "Helm Deploy Buffer to Context" },
+-- { "<leader>khr", "<cmd>RemoveDeployment<CR>", desc = "Helm Remove Deployment From Buffer" },
+-- { "<leader>kht", "<cmd>HelmTemplateFromBuffer<CR>", desc = "Helm Template From Buffer" },
+-- { "<leader>khu", "<cmd>HelmDependencyUpdateFromBuffer<CR>", desc = "Helm Dependency Update" },
+-- -- Kubectl Commands
+-- { "<leader>kk", group = "Kubectl" },
+-- { "<leader>kkC", "<cmd>SelectSplitCRD<CR>", desc = "Download CRD Split" },
+-- { "<leader>kkD", "<cmd>DeleteNamespace<CR>", desc = "Kubectl Delete Namespace" },
+-- { "<leader>kkK", "<cmd>terminal k9s<CR>", desc = "Open K9s" },
+-- { "<leader>kka", "<cmd>KubectlApplyFromBuffer<CR>", desc = "Kubectl Apply From Buffer" },
+-- { "<leader>kkc", "<cmd>SelectCRD<CR>", desc = "Download CRD" },
+-- { "<leader>kkk", "<cmd>OpenK9sSplit<CR>", desc = "Split View K9s" },
+-- { "<leader>kkl", "<cmd>ToggleYamlHelm<CR>", desc = "Toggle YAML/Helm" },
+-- -- Logs Commands
+-- { "<leader>kl", group = "Logs" },
+-- { "<leader>klf", "<cmd>JsonFormatLogs<CR>", desc = "Format JSON" },
+-- { "<leader>klv", "<cmd>ViewPodLogs<CR>", desc = "View Pod Logs" },
+-- }
 
-require("which-key").add(kube_utils_mappings)
+-- require("which-key").add(kube_utils_mappings)
+
+-- local terminal_utils_mapping = {
+--   { "<leader>t", group = "Terminal" }, -- Main title for all Terminal related commands
+--   { "<leader>tk", ":tabnew | terminal k9s<CR>", desc = "Open Terminal with K9s in New Tab" },
+--   { "<leader>tt", ":tabnew | terminal<CR>", desc = "Open Terminal in New Tab" },
+-- }
+--
+-- require("which-key").add(terminal_utils_mapping)
