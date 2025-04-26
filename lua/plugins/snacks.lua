e5f0e36f7824bfe38c61335f095a09b7ad421354
Gennady Paleev <gv.paleev@gmail.com>
2025-04-26 03:40:53 +0300

    bumpversion

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,9 +1,9 @@
-return {
-  "snacks.nvim",
-  opts = function(_, opts)
-    -- убираем логотип
-    opts.dashboard.preset.header = {}
-
-    return opts
-  end,
-}
+-- return {
+--   "snacks.nvim",
+--   opts = function(_, opts)
+--     -- убираем логотип
+--     opts.dashboard.preset.header = {}
+--
+--     return opts
+--   end,
+-- }
